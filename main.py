4642f1df76316570b477a3b0e13d718b9cc65f0b
f1destatistiques-dotcom <f1destatistiques@gmail.com>
2026-04-13 16:57:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ import streamlit as st
 import pandas as pd
 
 url = "https://raw.githubusercontent.com/f1destatistiques-dotcom/LMU-stats/refs/heads/main/records.csv?token=GHSAT0AAAAAAD2KFXB5PESA7H4D5Z7MAXE42O5AG5Q"
-#df = pd.read_csv(url)
-token = st.secrets["GITHUB_TOKEN"]
-
-df = pd.read_csv(url, storage_options={"Authorization": f"token {token}"})
+df = pd.read_csv(url)
 
 st.dataframe(df)
 
