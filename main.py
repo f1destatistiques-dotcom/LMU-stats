25743d766b2db3abb5d193a395b0f352c1fde6a1
f1destatistiques-dotcom <f1destatistiques@gmail.com>
2026-04-13 16:46:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+import pandas as pd
+
+url = "https://raw.githubusercontent.com/f1destatistiques-dotcom/LMU-stats/refs/heads/main/records.csv?token=GHSAT0AAAAAAD2KFXB5PESA7H4D5Z7MAXE42O5AG5Q"
+df = pd.read_csv(url)
+
+st.dataframe(df)
 
 st.title("Hi everyone, i'm one the place")
 st.header("Ici un header")
