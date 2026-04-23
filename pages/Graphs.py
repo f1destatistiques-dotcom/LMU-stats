22a3dccf9a14e53abaf6875a39b6f90d04ba1c59
f1destatistiques-dotcom <f1destatistiques@gmail.com>
2026-04-24 00:09:54 +0200

    Update Graphs.py

--- pages/Graphs.py
+++ pages/Graphs.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
+
 
 st.set_page_config(layout="wide") # Permet un affichage en plein écran
 
@@ -12,7 +15,9 @@ df_lap_by_circuit = pd.read_csv(url_lap_by_circuit)
 # df_sorted = df_lap_by_circuit.sort_values("Tours", ascending=True)
 # st.bar_chart(df_sorted.set_index("Circuit")["Tours"])
 
-import plotly.express as px
+
+# GRAPH DU NOMBRE DE TOURS PAR CIRCUITS______________________________________________
+
 df_sorted = df_lap_by_circuit.sort_values("Tours", ascending=False)
 
 fig = px.bar(
@@ -28,3 +33,35 @@ fig.update_layout(height=800)
 fig.update_traces(width=0.4)
 
 st.plotly_chart(fig, use_container_width=True)
+
+
+# GRAPH DE LA POSITION D'ARRIVÉE DES 20 DERNIÈRES COURSES______________________________________________
+
+url_last_20_races = "https://raw.githubusercontent.com/f1destatistiques-dotcom/LMU-stats/refs/heads/main/last_20.csv"
+df_last_20_races = pd.read_csv(url_last_20_races)
+
+fig = go.Figure()
+
+fig.add_trace(go.Scatter(
+    x = df_last_20_races["Course"],
+    y = df_last_20_races["Position"],
+    mode="lines+markers",
+    line=dict(color="#5784BA", width=3),
+    marker=dict(size=10, color="#4B79B1"),
+    fill="tozeroy",              # Remplissage sous la courbe
+    line_shape="spline",         # Courbe arrondie
+    name="Position"
+))
+
+# # --- Labels séparés (toujours visibles) ---
+# fig.add_trace(go.Scatter(
+#     x=df_last_20_races["Course"],
+#     y=df_last_20_races["Position"],
+#     mode="text",
+#     text=df_last_20_races["Position"],
+#     textposition="top center",
+#     textfont=dict(color="black", size=14),
+#     showlegend=False
+# ))
+
+st.plotly_chart(fig, config = {'scrollZoom': False})
